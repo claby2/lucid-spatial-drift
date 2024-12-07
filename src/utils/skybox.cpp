ed9bf6a8a6d7c1961a74f503e94ce5927302674e
Michael Rehmet <mrehmet@Michaels-MacBook-Pro-7.local>
2024-12-06 20:10:58 -0500

    ooo space

--- src/utils/skybox.cpp
+++ src/utils/skybox.cpp
@@ -4,7 +4,6 @@
 
 // faces is a vector of filepaths to the faces of the skybox
 void Skybox::initialize() {
-  glActiveTexture(GL_TEXTURE0);
     m_shader = ShaderLoader::createShaderProgram(
         ":/resources/shaders/skybox.vert", ":/resources/shaders/skybox.frag");
 
@@ -14,27 +13,27 @@ void Skybox::initialize() {
     QString posx = QString(":/resources/skyboxImages/px.png");
     m_image1 = QImage(posx);
     m_image1 = m_image1.convertToFormat(QImage::Format_RGBA8888).mirrored();
-    glTexImage2D(GL_TEXTURE_CUBE_MAP_POSITIVE_X, 0, GL_RGB, m_image1.width(), m_image1.height(), 0, GL_RGB, GL_UNSIGNED_BYTE, m_image1.bits());
+    glTexImage2D(GL_TEXTURE_CUBE_MAP_POSITIVE_X, 0, GL_RGBA, m_image1.width(), m_image1.height(), 0, GL_RGBA, GL_UNSIGNED_BYTE, m_image1.bits());
     QString negx = QString(":/resources/skyboxImages/nx.png");
     m_image2 = QImage(negx);
     m_image2 = m_image2.convertToFormat(QImage::Format_RGBA8888).mirrored();
-    glTexImage2D(GL_TEXTURE_CUBE_MAP_NEGATIVE_X, 0, GL_RGB, m_image2.width(), m_image2.height(), 0, GL_RGB, GL_UNSIGNED_BYTE, m_image2.bits());
+    glTexImage2D(GL_TEXTURE_CUBE_MAP_NEGATIVE_X, 0, GL_RGBA, m_image2.width(), m_image2.height(), 0, GL_RGBA, GL_UNSIGNED_BYTE, m_image2.bits());
     QString posy = QString(":/resources/skyboxImages/py.png");
     m_image3 = QImage(posy);
     m_image3 = m_image3.convertToFormat(QImage::Format_RGBA8888).mirrored();
-    glTexImage2D(GL_TEXTURE_CUBE_MAP_POSITIVE_Y, 0, GL_RGB, m_image3.width(), m_image3.height(), 0, GL_RGB, GL_UNSIGNED_BYTE, m_image3.bits());
+    glTexImage2D(GL_TEXTURE_CUBE_MAP_POSITIVE_Y, 0, GL_RGBA, m_image3.width(), m_image3.height(), 0, GL_RGBA, GL_UNSIGNED_BYTE, m_image3.bits());
     QString negy = QString(":/resources/skyboxImages/ny.png");
     m_image4 = QImage(negy);
     m_image4 = m_image4.convertToFormat(QImage::Format_RGBA8888).mirrored();
-    glTexImage2D(GL_TEXTURE_CUBE_MAP_NEGATIVE_Y, 0, GL_RGB, m_image4.width(), m_image4.height(), 0, GL_RGB, GL_UNSIGNED_BYTE, m_image4.bits());
+    glTexImage2D(GL_TEXTURE_CUBE_MAP_NEGATIVE_Y, 0, GL_RGBA, m_image4.width(), m_image4.height(), 0, GL_RGBA, GL_UNSIGNED_BYTE, m_image4.bits());
     QString posz = QString(":/resources/skyboxImages/pz.png");
     m_image5 = QImage(posz);
     m_image5 = m_image5.convertToFormat(QImage::Format_RGBA8888).mirrored();
-    glTexImage2D(GL_TEXTURE_CUBE_MAP_POSITIVE_Z, 0, GL_RGB, m_image5.width(), m_image5.height(), 0, GL_RGB, GL_UNSIGNED_BYTE, m_image5.bits());
+    glTexImage2D(GL_TEXTURE_CUBE_MAP_POSITIVE_Z, 0, GL_RGBA, m_image5.width(), m_image5.height(), 0, GL_RGBA, GL_UNSIGNED_BYTE, m_image5.bits());
     QString negz = QString(":/resources/skyboxImages/nz.png");
     m_image6 = QImage(negz);
     m_image6 = m_image6.convertToFormat(QImage::Format_RGBA8888).mirrored();
-    glTexImage2D(GL_TEXTURE_CUBE_MAP_NEGATIVE_Z, 0, GL_RGB, m_image6.width(), m_image6.height(), 0, GL_RGB, GL_UNSIGNED_BYTE, m_image6.bits());
+    glTexImage2D(GL_TEXTURE_CUBE_MAP_NEGATIVE_Z, 0, GL_RGBA, m_image6.width(), m_image6.height(), 0, GL_RGBA, GL_UNSIGNED_BYTE, m_image6.bits());
 
     glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
     glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
@@ -98,7 +97,7 @@ void Skybox::initialize() {
     glBufferData(GL_ARRAY_BUFFER, skyboxVertices.size()*sizeof(GLfloat), skyboxVertices.data(), GL_STATIC_DRAW);
     glGenVertexArrays(1, &m_vao);
     glBindVertexArray(m_vao);
-  glEnableVertexAttribArray(0);
+    glEnableVertexAttribArray(0);
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat),
                           nullptr);
     glBindVertexArray(0);
